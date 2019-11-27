56c24daac9a47902f8ecbf991ec06c75c4f2dfd7
NITSAN Technologies <sanjay@nitsan.in>
2019-11-27 09:53:54 +0530

    [Feature] Support extented tweets and retweets
    https://github.com/nitsan-technologies/ns_twitter/pull/12

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,8 +1,8 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => '[NITSAN] Twitter plugin to display all your Tweets',
-	'description' => '[NITSAN] Twitter extension brings all your recent tweets to your TYPO3 site',
+	'title' => '[NITSAN] Twitter Plugin',
+	'description' => '[NITSAN] Twitter extension brings all your recent tweets to your TYPO3 site. Demo: https://demo.t3terminal.com/t3t-extensions/twitter Pro version: https://t3terminal.com/twitter-display-plugin-free/',
 	'category' => 'plugin',
 	'author' => 'T3:Bhavin Barad, T3:Keval Pandya, QA:Vandna Kalivada',
 	'author_email' => 'sanjay@nitsan.in',
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'uploadfolder' => '0',
 	'createDirs' => '',
 	'clearCacheOnLoad' => 0,
-	'version' => '2.0.2',
+	'version' => '2.1.0',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '7.0.0-9.5.99',
