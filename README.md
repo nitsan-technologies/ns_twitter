53946b775494ef7e06ff2e11eda17fbac70c2a63
NITSAN <sanjay@nitsan.in>
2019-12-02 18:42:59 +0530

    [TODO] Adding Gitlab-CI & Travis with PHP, TypoScript, YAML lints [TODO] Updating documentation [TODO] Added demo link [TODO] Releasing pro-version of the extenion at https://t3terminal.com/

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ns_twitter
-The TYPO3 Extension: https://extensions.typo3.org/extension/ns_twitter/
+
+- FREE version: You can download FREE version with basic-features at https://extensions.typo3.org/extension/ns_twitter/
+- PRO version: You can download PRO version with more-features & free-support at https://t3terminal.com/twitter-display-plugin-free/
--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => '[NITSAN] Twitter Plugin',
-	'description' => '[NITSAN] Twitter extension brings all your recent tweets to your TYPO3 site. Demo: https://demo.t3terminal.com/t3t-extensions/twitter Pro version: https://t3terminal.com/twitter-display-plugin-free/',
+	'description' => 'Twitter extension brings all your recent tweets to your TYPO3 site. Demo: https://demo.t3terminal.com/t3t-extensions/twitter Pro version: You can download PRO version for more-features & free-support at https://t3terminal.com/twitter-display-plugin-free/',
 	'category' => 'plugin',
 	'author' => 'T3:Bhavin Barad, T3:Keval Pandya, QA:Vandna Kalivada',
 	'author_email' => 'sanjay@nitsan.in',
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'uploadfolder' => '0',
 	'createDirs' => '',
 	'clearCacheOnLoad' => 0,
-	'version' => '2.1.0',
+	'version' => '2.1.1',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '7.0.0-9.5.99',
