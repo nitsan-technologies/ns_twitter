95befee42804387408cce6e784c55bcedaf62a88
Nitsan Technologies <sanjay@nitsan.in>
2020-02-08 17:44:57 +0530

    [FIXED] Bug Fix

--- Classes/Controller/TweetController.php
+++ Classes/Controller/TweetController.php
@@ -131,6 +131,7 @@ class TweetController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
 							}
 							$results[$key]['full_text_raw'] = $results[$key]['full_text'];
 							$results[$key]['full_text'] = $this->convert_links($results[$key]['full_text']);
+							$results[$key]['text'] = $results[$key]['full_text'];
 						} 
 
 						$results[$key]['created_at'] = $resultdate;
