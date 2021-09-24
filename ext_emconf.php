e9abec0bc2b3e996fbef3d0fde0c168c057d9d41
Pradeepsinh <pradeep.nitsan@gmail.com>
2021-09-24 11:51:45 +0530

    Upload Plugin Icon

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,25 +1,25 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-    'title' => '[NITSAN] Twitter Plugin',
-    'description' => 'Twitter extension brings all your recent tweets to your TYPO3 site. Demo: https://demo.t3terminal.com/t3t-extensions/twitter Pro version: You can download PRO version for more-features & free-support at https://t3terminal.com/twitter-display-plugin-free/',
-    'category' => 'plugin',
-    'author' => 'NITSAN Technologies Pvt Ltd',
-    'author_email' => 'sanjay@nitsan.in',
-    'author_company' => 'NITSAN Technologies Pvt Ltd',
-    'state' => 'stable',
-    'internal' => '',
-    'uploadfolder' => '0',
-    'createDirs' => '',
-    'clearCacheOnLoad' => 0,
-    'version' => '2.2.4',
-    'constraints' => [
-        'depends' => [
-            'typo3' => '7.0.0-10.9.99',
-        ],
-        'conflicts' => [
-        ],
-        'suggests' => [
-        ],
-    ],
+	'title' => '[NITSAN] Twitter Plugin',
+	'description' => 'Twitter extension brings all your recent tweets to your TYPO3 site. Demo: https://demo.t3terminal.com/t3t-extensions/twitter Pro version: You can download PRO version for more-features & free-support at https://t3terminal.com/twitter-display-plugin-free/',
+	'category' => 'plugin',
+	'author' => 'NITSAN Technologies Pvt Ltd',
+	'author_email' => 'sanjay@nitsan.in',
+	'author_company' => 'NITSAN Technologies Pvt Ltd',
+	'state' => 'stable',
+	'internal' => '',
+	'uploadfolder' => '0',
+	'createDirs' => '',
+	'clearCacheOnLoad' => 0,
+	'version' => '2.2.5',
+	'constraints' => [
+		'depends' => [
+			'typo3' => '7.0.0-10.9.99',
+		],
+		'conflicts' => [
+		],
+		'suggests' => [
+		],
+	],
 ];
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,36 +1,36 @@
 <?php
 if (!defined('TYPO3_MODE')) {
-    die('Access denied.');
+	die('Access denied.');
 }
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'Nitsan.ns_twitter',
-    'Recenttweets',
-    [
-        'Tweet' => 'list',
+	'Nitsan.ns_twitter',
+	'Recenttweets',
+	[
+		'Tweet' => 'list',
 
-    ],
-    // non-cacheable actions
-    [
-        'Tweet' => '',
+	],
+	// non-cacheable actions
+	[
+		'Tweet' => '',
 
-    ]
+	]
 );
 if (version_compare(TYPO3_branch, '7.0', '>')) {
-    if (TYPO3_MODE === 'BE') {
-        $icons = [
-            'twitter-plugin' => 'twitter-plugin.svg',
-        ];
-        $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-        foreach ($icons as $identifier => $path) {
-            $iconRegistry->registerIcon(
-                $identifier,
-                \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-                ['source' => 'EXT:ns_twitter/Resources/Public/Icons/' . $path]
-            );
-        }
-    }
+	if (TYPO3_MODE === 'BE') {
+		$icons = [
+			'twitter-plugin' => 'ns_twitter.svg',
+		];
+		$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+		foreach ($icons as $identifier => $path) {
+			$iconRegistry->registerIcon(
+				$identifier,
+				\TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+				['source' => 'EXT:ns_twitter/Resources/Public/Icons/' . $path]
+			);
+		}
+	}
 }
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem']['ns_twitter']
-        = \Nitsan\NsTwitter\Hooks\PageLayoutView::class;
+= \Nitsan\NsTwitter\Hooks\PageLayoutView::class;
