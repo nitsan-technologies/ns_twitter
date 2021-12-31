3e484ff5c8586f485b3821328431a4f327c2defa
NITSAN Technologies <sanjay@nitsan.in>
2021-12-31 13:33:47 +0530

    [TASK] Corrected the $EM_CONF with key-name

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,10 +1,10 @@
 <?php
 
-$EM_CONF[$_EXTKEY] = [
+$EM_CONF['ns_twitter'] = [
 	'title' => '[NITSAN] Twitter Plugin',
-	'description' => 'Twitter extension brings all your recent tweets to your TYPO3 site. Demo: https://demo.t3terminal.com/t3t-extensions/twitter Pro version: You can download PRO version for more-features & free-support at https://t3terminal.com/twitter-display-plugin-free/',
+	'description' => 'Twitter extension brings all your recent tweets to your TYPO3 site. LiveDemo: https://demo.t3terminal.com/t3t-extensions/twitter Pro version: You can download PRO version for more-features & free-support at https://t3terminal.com/twitter-display-plugin-free/',
 	'category' => 'plugin',
-	'author' => 'NITSAN Technologies Pvt Ltd',
+	'author' => 'Team NITSAN',
 	'author_email' => 'sanjay@nitsan.in',
 	'author_company' => 'NITSAN Technologies Pvt Ltd',
 	'state' => 'stable',
